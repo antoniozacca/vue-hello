48d9dac183fdbed0304aadf000e2f5842febec8e
antoniozacca <antoniozaccaria057@gmail.com>
2024-05-20 14:45:32 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            message: "Hello",
+            image: "https://www.b2x.it/rest/images/2023/11/15/1488336.jpg?imageFormat=@1x"
+        }
+    }
+}).mount("#app")
